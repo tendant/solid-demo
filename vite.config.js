69769fa2996a4669bb0ead085d0f321165ba44a1
Lei Wang <tendant@gmail.com>
2023-03-11 23:51:46 -0800

    add proxy for server

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,18 @@ export default defineConfig({
   plugins: [solidPlugin()],
   server: {
     port: 3000,
+    proxy: {
+      "^/api/": {
+        target: "http://127.0.0.1:4000",
+        ws: true,
+        changeOrigin: true
+      },
+      "^/auth/": {
+        target: "http://127.0.0.1:4000",
+        ws: true,
+        changeOrigin: true
+      },
+    },
   },
   build: {
     target: 'esnext',
